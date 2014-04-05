892687ba60f1ce274fd9fa87c148b974c3d7bc9c
Maurice Williams <morficus@gmail.com>
2014-04-05 12:06:23 -0400

    Adding the real meat of this project - the facade and autobinder files. Still need to test it in AMD-mode

---
+++ omniture-autobinder.js
@@ -0,0 +1,190 @@
+(function(root, factory) {
+
+    // Set up omniture-autobinder for either AMD or non-AMD environments
+    if (typeof define === 'function' && define.amd) {
+        define(['omniture-facade', 'underscore', 'jquery'], function(Analytics, _, $) {
+            // Export global even in AMD case in case this script is loaded with
+            // others that may still expect a global Backbone.
+            factory(root, Analytics, _, $);
+        });
+
+    } else {
+        factory(root, root.Analytics, root._, (root.jQuery  || root.$));
+    }
+
+}(this, function(root, Analytics, _, $){
+
+    /**
+     * Pretty much just a collection of setters.
+     * Helper functions to set various omniture properties
+     */
+    var AnalyticsPropertySetter = {
+        download: function(text, $el){
+            this._setLinkType('d', text, $el);
+        },
+
+        exit: function(text, $el){
+            this._setLinkType('e', text, $el);
+        },
+
+        other: function(text, $el){
+            this._setLinkType('o', text, $el);
+        },
+
+        _setLinkType: function(type, text, $el){
+            var data = {};
+            data.linkElement = $el.get(0);
+            data.linkName = text;
+            data.linkType = type;
+
+            if(type === 'd'){
+                //recordDownloadLink(data);
+                Analytics.recordDownloadLink(data);
+            }else if(type === 'e'){
+                //recordExitLink(data);
+                Analytics.recordExitLink(data);
+            }else if(type === 'o'){
+                //recordLocalLink(data);
+                Analytics.recordLocalLink(data);
+            }
+        },
+
+        video: function(){
+            //TODO: still need to figure this one out...
+        },
+
+        pageView: function(){
+            Analytics.recordPageView();
+        },
+
+        setSectionName: function(text){
+            Analytics.setSectionName(text);
+        },
+
+        setPageName: function(text){
+            Analytics.setPageName(text);
+        },
+        /**
+         * Sets all eVar values based on the values passed in propArray
+         * @param evarArray An array of numbers
+         * @param evarText Text to be used when recording the eVars
+         */
+        setEvars: function(evarArray, evarText){
+            _.each(evarArray, function(evarIndex){
+                var evarName = 'evar' + evarIndex;
+                s[evarName] = evarText;
+            });
+        },
+
+        /**
+         * Sets all prop values based on the values passed in propArray
+         * @param propArray An array of numbers
+         * @param propText Text to be used when recording the prop values
+         */
+        setProps: function(propArray, propText){
+            _.each(propArray, function(propIndex){
+                var propName = 'prop' + propIndex;
+                s[propName] = propText;
+            });
+        },
+
+        /**
+         * Sets all event values based on the values passed in eventArray
+         * @param eventArray An array of numbers
+         * @param eventText Text to be used when recording the event
+         */
+        setEvents: function(eventArray, eventText){
+            _.each(eventArray, function(eventIndex){
+                var eventName = 'event' + eventIndex;
+                s[eventName] = eventText;
+            });
+        }
+    };
+
+    /**
+     * (Intended to always be used as a callback)
+     * Inspects the element which triggered the event and determines what data needs to be sent over to Omniture
+     * @param event Click event
+     */
+    var analyticsHandlerCallback = function(event){
+        //extract all sorts of data from the target element
+        var $target = $(event.target);
+
+
+        //if the immediate target is not omniture-tagged, check if any of its parents are and re-define the target
+        if($target.attr('data-omniture') === undefined){
+            var newTarget = $target.parents('data-omniture')[0];
+            //the original normal nor any of his parents are omniture-tagged... so we can just bail
+            if(newTarget === undefined){
+                return;
+            }else{
+                //otherwise wrap it in some jQuery-goodness
+                $target = $(newTarget);
+            }
+        }
+
+            //the text to be sent back to omniture
+        var elementText = $target.data('omniture-text') || $target.text(),
+            //the type of link, acceptable values are: download, other, exit, video, pageView, sectionName, channelName
+            omniturePropertyType = $target.data('omniture'),
+            pageName = $target.data('omniture-pagename'),
+            sectionName = $target.data('omniture-channelname'),
+            //a comma-seprated list of numbers. would indicate which eVar, prop or event to use (ie: eVar1)
+            evars = $target.data('omniture-evar'),
+            props = $target.data('omniture-prop'),
+            events = $target.data('omniture-event');
+
+        //the element has to AT LEAST have an property/link type OR some type of evar, prop or event. if it doesn't have
+        //any of that... then there is nothing to send to Omniture
+        if( _.isEmpty(omniturePropertyType) && ( _.isEmpty(evars) && _.isEmpty(props) && _.isEmpty(events)) ){
+            return;
+        }
+
+        //remove any type of extranuous white spaces
+        elementText = $.trim(elementText);
+
+        //set custom variables, events and stuff...
+        if(evars){
+            evars = evars.toString();
+            AnalyticsPropertySetter.setEvars(evars.split(','), elementText);
+        }
+
+        if(props){
+            props = props.toString();
+            AnalyticsPropertySetter.setProps(props.split(','), elementText);
+        }
+
+        if(events){
+            events = events.toString();
+            AnalyticsPropertySetter.setEvents(events.split(','), elementText);
+        }
+
+
+        if(sectionName){
+            AnalyticsPropertySetter.setSectionName(sectionName);
+        }
+
+        if(pageName){
+            AnalyticsPropertySetter.setPageName(pageName);
+        }
+
+        try{
+            if(AnalyticsPropertySetter.hasOwnProperty(omniturePropertyType)){
+                AnalyticsPropertySetter[omniturePropertyType](elementText, $target);
+            }else{
+                console.warn('Undefined Onmniture action: ' + omniturePropertyType);
+            }
+        }catch(error){
+            throw error;
+        }
+    };
+
+    //Binding do the ```document``` tag gives the benefit of being able to capture all clicks all the time, even if the
+    //element is dynamically added after this fired. It's all thanks to the magic of event-bubbling.
+    //We also want to throttle all the calls, in case the user goes on a crazy click-frenzy
+    var throtteledCallback = _.throttle(analyticsHandlerCallback, 500, {trailing: false});
+    $(document).on('click', throtteledCallback);
+
+    console.log('auto-binding Omniture magic in on');
+
+}));
---
+++ omniture-facade.js
@@ -0,0 +1,172 @@
+(function(root, factory) {
+
+    // Set up omniture-autobinder for either AMD or non-AMD environments
+    if (typeof define === 'function' && define.amd) {
+        define(['omniture', 'underscore'], function(s, _) {
+            // Export global even in AMD case in case this script is loaded with
+            // others that may still expect a global Backbone.
+            root.Analytics = factory(root, s, _);
+        });
+
+    } else {
+        root.Analytics = factory(root, root.s, root._);
+    }
+
+}(this, function(root, s, _){
+
+    return {
+        pageName: '',
+        sectionName: '',
+
+        /**
+         * Sets the section name and removes the page name
+         * @param {String} section Section name
+         */
+        setSectionName: function (section) {
+            this.sectionName = section;
+            //since we are switching sections/channels, it is assumed that the page name must not stay the same
+            this.setPageName('');
+        },
+
+        /**
+         * Sets the page name
+         * @param {String} pagename Page name
+         */
+        setPageName: function (pagename) {
+            this.pageName = pagename;
+
+        },
+
+        /**
+         * Sets a users unique identifier.
+         * If this is not set, Omniture will generate a unique ID for them anyways.
+         * @param {String} guid A string representing a users unique identifier.
+         *
+         */
+        setGuid: function (guid) {
+            //strip hyphens from the GUID, and set it as the visitor ID
+            s.visitorID = guid.replace(/-/g, '');
+            return true;
+        },
+
+        /**
+         * Creates a normal page-view record.
+         * Before calling this, you will want to make sure you have called setSectionName() and/or setPageName()
+         */
+        recordPageView: function () {
+
+            s.channel = this.sectionName;
+
+            if ( (this.sectionName !== '' && this.sectionName !== undefined) && (this.pageName !== '' && this.pageName !== undefined) ) {
+
+                s.pageName = this.sectionName + ' | ' + this.pageName;
+            } else {
+                s.pageName = this.pageName;
+            }
+            s.t();
+            this._cleanupVars();
+        },
+
+        /**
+         * Records a local link (aka: a jump link, with in the same page or section)
+         * @param  {Object} linkData Details about the link that was clicked
+         *                           linkData.linkName = the text to uniquely identify this link
+         *                           linkData.linkType = the type of omniture-link (defaults to other-link)
+         *                           linkData.linkElement = type of element linked (defaults to an anchor-tag)
+         */
+        recordLocalLink: function (linkData) {
+            var defaultLinkData = {
+                'linkEelement': 'a',
+                'linkType': 'o',
+                'linkName': '',
+                'variableOverrides': null,
+                'doneAction': ''
+            };
+
+            linkData = _.defaults(linkData, defaultLinkData);
+
+            this._recordLink(linkData);
+        },
+
+        /**
+         * Records a sites exit link
+         * @param  {Object} linkData Details about the link that was clicked
+         *                           linkData.linkName = the text to uniquely identify this link
+         *                           linkData.linkType = the type of omniture-link (defaults to exit-link)
+         *                           linkData.linkElement = type of element linked (defaults to an anchor-tag)
+         */
+        recordExitLink: function (linkData) {
+            var defaultLinkData = {
+                'linkElement': 'a',
+                'linkType': 'e',
+                'linkName': '',
+                'variableOverrides': null,
+                'doneAction': ''
+            };
+
+            linkData = _.defaults(linkData, defaultLinkData);
+
+            this._recordLink(linkData);
+        },
+
+        /**
+         * Records a sites exit link
+         * @param  {Object} linkData Details about the link that was clicked
+         *                           linkData.linkName = the text to uniquely identify this link
+         *                           linkData.linkType = the type of omniture-link (defaults to exit-link)
+         *                           linkData.linkElement = type of element linked (defaults to an anchor-tag)
+         */
+        recordDownloadLink: function (linkData) {
+            var defaultLinkData = {
+                'linkElement': 'a',
+                'linkType': 'd',
+                'linkName': '',
+                'variableOverrides': null,
+                'doneAction': ''
+            };
+
+            linkData = _.defaults(linkData, defaultLinkData);
+
+            this._recordLink(linkData);
+        },
+
+        /**
+         * Deletes all evars, props and event data that was previously set
+         * @private
+         */
+        _cleanupVars: function(){
+            var count = 50;
+            for(var i = 1; i <= count; i++){
+                delete s['evar' + i];
+                delete s['prop' + i];
+                delete s['event' + i];
+            }
+        },
+
+        _recordLink: function (linkData) {
+
+            var defaultLinkData = {
+                'linkEelement': '',
+                'linkType': '',
+                'linkName': '',
+                'variableOverrides': null,
+                'doneAction': '',
+                'linkurl': ''
+            };
+
+            linkData = _.defaults(linkData, defaultLinkData);
+
+            //ignore stuff that doesn't have all the necesary data
+            if (linkData.linkType === '' || linkData.linkName === '') {
+                return;
+            }
+
+            //only process URL's that are less than 8000 characters, else Omniture/SiteCatalyst will get pretty unhappy
+            if (linkData.linkurl.length < 8000) {
+                s.tl(linkData.linkElement, linkData.linkType, linkData.linkName);
+            }
+        }
+
+    };
+
+}));
